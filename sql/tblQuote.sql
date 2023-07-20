f3882f53de0f8dba5635facf4c36179e4d1acb91
kazak.ke <KazakKE@str.mos.ru>
2023-07-20 17:47:46 +0300

    project restructuring

--- sql/tblQuote.sql
+++ sql/tblQuote.sql
@@ -1,4 +1,47 @@
+CREATE TABLE IF NOT EXISTS tblQuotes
+                (
+                    ID_tblQuotes INTEGER PRIMARY KEY,
+                    catalog TEXT NOT NULL,
+                    cod TEXT NOT NULL,
+                    name TEXT NOT NULL,
+                    item INTEGER,
+                    metric TEXT,
+                    stat_sum INTEGER DEFAULT NULL,
+                    class TEXT DEFAULT NULL,
+                    parent TEXT DEFAULT NULL,
+                    UNIQUE (cod)
+                );
 
+CREATE TABLE IF NOT EXISTS tblValueDimensions
+                (
+                    ID_tblValueDimensions INTEGER PRIMARY KEY,
+                    min_value INTEGER DEFAULT 0,
+                    max_value INTEGER DEFAULT 0,
+                    unit_dimension TEXT,
+                    step_dimension TEXT,
+                    parameter_type INTEGER DEFAULT 0,
+                    FK_tblDimensions_tblValueDimensions INTEGER DEFAULT 0,
+                    FOREIGN KEY (FK_tblDimensions_tblValueDimensions) REFERENCES tblDimensions (ID_tblDimensions),
+
+                    UNIQUE (FK_tblDimensions_tblValueDimensions, min_value, max_value, unit_dimension, step_dimension, parameter_type)
+                );
+
+CREATE TABLE IF NOT EXISTS tblDimensions
+                (
+                    ID_tblDimensions INTEGER PRIMARY KEY,
+                    name TEXT,
+                    UNIQUE (name)
+                );
+
+CREATE TABLE IF NOT EXISTS tblDimensionSets
+                (
+                    ID_tblDimensionSets INTEGER PRIMARY KEY,
+                    FK_tblQuotes_tblDimensionSets INTEGER DEFAULT 0,
+                    FK_tblValueDimensions_tblDimensionSets INTEGER DEFAULT 0,
+                    FOREIGN KEY (FK_tblQuotes_tblDimensionSets) REFERENCES tblQuotes (ID_tblQuotes),
+                    FOREIGN KEY (FK_tblValueDimensions_tblDimensionSets) REFERENCES tblValueDimensions (ID_tblValueDimensions),
+                    UNIQUE (FK_tblQuotes_tblDimensionSets, FK_tblValueDimensions_tblDimensionSets)
+                );
 
 
 SELECT * FROM tblQuotes WHERE item = 0;
--- try/quotes_to_db.py
+++ try/quotes_to_db.py
@@ -33,6 +33,36 @@ print(f"непустых значений в столбце 'A': {data.df[data.d
 digit_pattern = re.compile(r"^\d+")
 
 
+def get_just_digits(src_data: str, pattern) -> tuple[int, str]:
+    src_data = src_data.strip()
+    result = pattern.match(src_data)
+    if result:
+        number: int = int(src_data[result.span()[0]:result.span()[1]])
+        text: str = src_data[result.span()[1]:].strip()
+        return number, text
+    return 0, src_data
+
+
+def get_quote(row) -> tuple | None:
+    result = list()
+    for column_name in ["B", "C", "D", "E", "F", "H", "I"]:
+        column = data.column_number.get(column_name, None)
+        value = data.cell_value(row, column)
+        if value:
+            result.append(value)
+        else:
+            result.append(None)
+    if None in result[:3]:                      # есть пустые значения в столбцах B, C, D
+        print(f"плохая расценка: {result}")
+        return None
+    else:
+        index_column_e = 3
+        unit = get_just_digits(result[index_column_e], digit_pattern) if result[index_column_e] else (0, "")
+        result.pop(3)
+        result[3:2] = list(unit)
+        return tuple(result)
+
+
 
 def main():
     # for row in range(1, data.row_max+1):
